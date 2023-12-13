fc4dabeb4493b42fb161a411e954ee62bdc0ac9b
VietBx23 <Vietbx23@gmail.com>
2023-12-13 20:33:00 +0700

    Viet: Giup Hau Address

--- src/main/resources/static/assets/admin2/js/address-ctrl.js
+++ src/main/resources/static/assets/admin2/js/address-ctrl.js
@@ -1,11 +1,16 @@
 app.controller("address-ctrl", function($scope, $http) {
 	$scope.items = [];
 	$scope.form = {};
+	$scope.city = [];
+	$scope.districts = [];
+	$scope.wards = [];
 	const host = "https://provinces.open-api.vn/api/";
 	var callAPI = (api) => {
 		return axios.get(api)
 			.then((response) => {
-				renderData(response.data, "province");
+				renderData(response.data, "city");
+
+
 			});
 	}
 	callAPI('https://provinces.open-api.vn/api/?depth=1');
@@ -29,61 +34,126 @@ app.controller("address-ctrl", function($scope, $http) {
 		});
 		document.querySelector("#" + select).innerHTML = row;
 	}
+	$("#city").change(() => {
+		let selectedOptionCity = $("#city option:selected");
+		console.log("Province changed. Selected Option:", selectedOptionCity.text());
 
-	$("#province").change(() => {
-		console.log("Province changed");
-		let selectedCode = $("#province option:selected").data("code");
+		let selectedCode = selectedOptionCity.data("code");
 		callApiDistrict(host + "p/" + selectedCode + "?depth=2");
+		$scope.city = selectedOptionCity.text();
+
+
 	});
+
 	$("#district").change(() => {
-		let selectedCode = $("#district option:selected").data("code");
+		let selectedOptionDistrict = $("#district option:selected");
+		console.log("District changed. Selected Option:", selectedOptionDistrict.text());
+		let selectedCode = selectedOptionDistrict.data("code");
 		callApiWard(host + "d/" + selectedCode + "?depth=2");
+
+
 	});
+
+	$("#ward").change(() => {
+		let selectedOptionWard = $("#ward option:selected");
+		console.log("Ward changed. Selected Option:", selectedOptionWard.text());
+
+	});
+
+
+
+
 	$scope.initialize = function() {
 		$http.get("/rest/address").then(resp => {
 			$scope.items = resp.data;
 			console.log(resp.data);
 			console.log(resp.data.addressDetail);
 			$scope.form = {
+
 				available: true,
+
+
 			};
 		})
+
+
+		$http.get("/rest/accounts").then(resp => {
+			$scope.accounts = resp.data;
+		});
 
 		$scope.reset(); //để có hình mây lyc1 mới đầu
 		$scope.loadCurrentUser();
 	}
-	$scope.edit = function(item) {
-		$scope.form = angular.copy(item);
 
-	}
 	$scope.create = function() {
-    var item = angular.copy($scope.form);
-    $http.post(`/rest/address`, item).then(resp => {
-        $scope.items.push(resp.data);
-          console.log($scope.items);
-        $scope.reset();
+		/*	console.log($("#province option:selected").text());
+			console.log($("#district option:selected").text());
+			console.log($("#ward option:selected").text());
+			console.log("Form data before copy:", $scope.form);*/
 
-        // Thêm code gọi API cập nhật addressdetails
-        var newAddressId = resp.data.id;
-        var addressDetailsData = {
-            // Dữ liệu cần cập nhật vào addressdetails
-            addressId: newAddressId,
-            // Thêm các trường dữ liệu khác nếu cần
-        };
+		var item = angular.copy($scope.form);
 
-        $http.post(`/rest/addressdetails`, addressDetailsData).then(resp => {
-            // Cập nhật addressdetails thành công
-        }).catch(error => {
-            // Xử lý lỗi khi cập nhật addressdetails
-            console.log("Error updating addressdetails", error);
-        });
+		item.city = ($("#city option:selected").text());
+		item.district = ($("#district option:selected").text());
+		item.ward = ($("#ward option:selected").text());
+		let streetInput = $('#address');
+		item.street = streetInput.val();
 
-        alert("Thêm mới thành công!");
-    }).catch(error => {
-        alert("Thêm địa chỉ không thành công");
-        console.log("Error", error);
-    });
+		item.addressDetail = (streetInput.val() + "," + $("#ward option:selected").text() + "," + $("#district option:selected").text() + "," + $("#province option:selected").text());
+		console.log(item.city);
+		console.log(item.district);
+		console.log(item.ward);
+
+		// Check if the necessary address information is present
+		if (item.city && item.district && item.ward) {
+
+
+			$http.post(`/rest/address`, item).then(resp => {
+				$scope.items.push(resp.data);
+
+				$scope.reset();
+
+				// Thêm code gọi API cập nhật addressdetails
+				var newAddressId = resp.data.id;
+				var addressDetailsData = {
+					addressId: newAddressId,
+					// Add other fields if needed
+				};
+
+				$http.post(`/rest/addressdetails`, addressDetailsData).then(resp => {
+					// Cập nhật addressdetails thành công
+				}).catch(error => {
+					console.log("Error updating addressdetails", error);
+				});
+
+				alert("Thêm mới thành công!");
+			}).catch(error => {
+				alert("Thêm địa chỉ không thành công");
+				console.log("Error", error);
+			});
+		} else {
+			alert("Vui lòng chọn đầy đủ thông tin địa chỉ (province, district, ward) trước khi tạo.");
+		}
+	}
+
+$scope.edit = function(item) {
+    // Copy the properties of the selected item to $scope.form
+    $scope.form = angular.copy(item);
+
+    // Manually set the selected values for the dropdowns
+    $scope.form.city = item.city;
+    $scope.form.district = item.district;
+    $scope.form.ward = item.ward;
+    
+    
+    
+    $("#province option:selected").text();
+   
+   
+   
+
 }
+
 
 	$scope.loadCurrentUser = function() {
 		$http.get("/rest/accounts/current-account").then(resp => {
@@ -158,6 +228,6 @@ app.controller("address-ctrl", function($scope, $http) {
 		}
 	}
 	$scope.initialize();
-	
+
 })
 
